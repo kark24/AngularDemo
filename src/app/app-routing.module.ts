dcaf6d10406e3a3e0b1db709091a4cda6dda8d88
manoj10101996 <manoj10101996@gmail.com>
2019-08-24 21:03:18 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ViewComponent } from './view/view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent }
+  , { path: "view", component: ViewComponent },
+  { path: "**", redirectTo: "login" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
